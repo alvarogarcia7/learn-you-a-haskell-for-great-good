2251023ccac5e93cde7cd274b1a855da14ca279a
Alvaro Garcia <derecerca@gmail.com>
2015-07-28 21:07:44 +0200

    refactor: prefer putting the tree parameter at the end so the function call can be composed with $

--- bst2.hs
+++ bst2.hs
@@ -3,12 +3,12 @@ data MyTree a = EmptyTree | Node a (MyTree a) (MyTree a) deriving Show
 singletonTree :: a -> MyTree a
 singletonTree a = Node a EmptyTree EmptyTree
 
-treeAdd :: (Ord a) => MyTree a -> a -> MyTree a
-treeAdd EmptyTree v = singletonTree v
-treeAdd (Node n left right) v
-    | v < n = nn (treeAdd left v) right
+treeAdd :: (Ord a) => a -> MyTree a -> MyTree a
+treeAdd v EmptyTree = singletonTree v
+treeAdd v (Node n left right)
+    | v < n = nn (treeAdd v left) right
     | v == n = nn left right
-    | v > n = nn left (treeAdd right v)
+    | v > n = nn left (treeAdd v right)
     where nn = Node n
 
 inOrderString :: (Show a) => MyTree a -> String
