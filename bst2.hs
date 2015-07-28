e436a41effde7a1280034e8df11e9a6ca6d164f6
Alvaro Garcia <derecerca@gmail.com>
2015-07-28 19:59:56 +0200

    refactor: rename parameter

--- bst2.hs
+++ bst2.hs
@@ -4,7 +4,7 @@ singletonTree :: a -> MyTree a
 singletonTree a = Node a EmptyTree EmptyTree
 
 treeAdd :: (Ord a) => MyTree a -> a -> MyTree a
-treeAdd EmptyTree x = singletonTree x
+treeAdd EmptyTree v = singletonTree v
 treeAdd (Node v left right) a
     | a < v = Node v (treeAdd left a) right
     | a == v = Node v left right
