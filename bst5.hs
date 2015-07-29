b97daf3cb93feb78f4e3f3844541fa89dd26707a
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-07-29 10:13:02 +0200

    add how to use this

--- bst5.hs
+++ bst5.hs
@@ -16,3 +16,8 @@ inOrder (N l n r) = (inOrder l) ++ [n] ++ (inOrder r)
 
 fromList :: (Ord v) => [v] -> T v
 fromList (head:tail) = foldr add (t head) (reverse tail)
+
+; how to use it
+; > t 1
+; > add 2 $ t 1
+; > inOrder $ add 2 $ t1
