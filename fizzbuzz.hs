80e33b68999470c3f7d56186ac2d94a02f07ff40
Alvaro Garcia <derecerca@gmail.com>
2015-08-14 23:37:01 +0200

    refactor: convert cycle to a function with its explanation

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -6,5 +6,6 @@ fizzbuzz n = take n $ zipWith ($) (cycle [s,s,f,s,b,f,s,s,f,b,s,f,s,s,fb]) [1..]
   b=const"buzz"
   fb=const"fizzbuzz"
 
--- cycle [1,2]
+binaryValues = cycle [1,2]
 -- generates list with [1,2,1,2,...]
+-- use it as: take 10 binaryValues
