0474358f2766ba756a0739ed98f992fb234f424e
Alvaro Garcia <derecerca@gmail.com>
2015-08-14 23:40:58 +0200

    add function repeatedNumbers

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -9,3 +9,7 @@ fizzbuzz n = take n $ zipWith ($) (cycle [s,s,f,s,b,f,s,s,f,b,s,f,s,s,fb]) [1..]
 binaryValues = cycle [1,2]
 -- generates list with [1,2,1,2,...]
 -- use it as: take 10 binaryValues
+
+-- generates an infinite list of repeated elements, removing each second value
+-- [1,1,3,3,5,5,...]
+repeatedNumbers = zipWith ($) (cycle [id,(\x -> x - 1)]) [1..]
