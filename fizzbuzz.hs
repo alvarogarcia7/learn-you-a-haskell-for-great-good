30524e93113d8468711d760e3b55eda5ba7e6013
Alvaro Garcia <derecerca@gmail.com>
2015-08-14 23:34:14 +0200

    add explanation for cycle function

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -5,3 +5,6 @@ fizzbuzz n = take n $ zipWith ($) (cycle [s,s,f,s,b,f,s,s,f,b,s,f,s,s,fb]) [1..]
   f=const"fizz"
   b=const"buzz"
   fb=const"fizzbuzz"
+
+-- cycle [1,2]
+-- generates list with [1,2,1,2,...]
